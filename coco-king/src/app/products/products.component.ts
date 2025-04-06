35c793bc4e8636cdecb017470de91a3cb2b48e3b
vivek-jadav <vivekjadav631@gmail.com>
2025-04-06 21:16:55 +0530

    .

--- coco-king/src/app/products/products.component.ts
+++ coco-king/src/app/products/products.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
+
+  @ViewChild('aboutContent') aboutContent: ElementRef | any;
+
+  ngOnInit(): void {
+    // Scroll to aboutContent position when the component is loaded
+    setTimeout(() => {
+      window.scrollTo(0, this.aboutContent.nativeElement.offsetTop / 2);
+    }, 0); // Delay to ensure the component is fully loaded
+  }
 
 }
